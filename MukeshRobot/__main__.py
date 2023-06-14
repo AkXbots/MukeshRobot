6fc33a7aa2fb598f8a8cfc191b98ce867652f5e5
AkXbots <122038135+AkXbots@users.noreply.github.com>
2023-06-14 20:06:38 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -237,7 +237,7 @@ def start(update: Update, context: CallbackContext):
             time.sleep(0.3)
             lol.delete()
             
-            update.effective_message.reply_text(PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME,sql.num_users(),sql.num_chats()),
+            update.effective_message.reply_text(PM_START_TEXT.format(escape_markdown(first_name), (START_IMG), BOT_NAME,sql.num_users(),sql.num_chats()),
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
