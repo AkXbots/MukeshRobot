150fcd0f5932f0f6a1622b4dee23200dcad67283
AkXbots <122038135+AkXbots@users.noreply.github.com>
2023-06-21 09:41:19 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -14,38 +14,38 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "" # integer value, dont use ""
-    API_HASH = ""
-    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = "2145093972" # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Mr_sukkun"
-    SUPPORT_CHAT = "the_support_chat"  # Your own group for support, do not add the @
+    API_ID = "26405281" # integer value, dont use ""
+    API_HASH = "77b54622ef8e0fd15555d939fc74005d"
+    TOKEN = "6164258122:AAHKqHRAi-p118EOrA9g9e6HF_v7e1U0Y30"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = "6058139652" # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "jashan_bal69x"
+    SUPPORT_CHAT = "Xd_Bots_Support"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001770762410
+        -1001655270746
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001770762410
+        -1001655270746
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-    MONGO_DB_URI= ""
+    MONGO_DB_URI= "mongodb+srv://sano07:sano07@cluster0.jxywgnn.mongodb.net/?retryWrites=true&w=majority"
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = ""  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = "postgres://ldgvebhopozkzy:6f707efe3a3f37f50c23bd64dde30d0037792810f04c742b0e3c1b9e523d681c@ec2-54-227-169-170.compute-1.amazonaws.com:5432/degk50kfgf6dn0"  # needed for any database modules
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = "09i2gqhAMVMR1E1HM2jX81FOjj_RJSuplJmdwb5ABK8OULB8qD6JUv5Mz8umicpg"  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "p08bVWlSmVyT~X_g11gqlbLnaJul4qh52OcVaJ8RAwAD4C~I91caPwL1hRH6Id_c"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = get_user_list("elevated_users.json", "2145093972")
+    DRAGONS = get_user_list("elevated_users.json", "5875450498")
     ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("elevated_users.json", "2145093972")
+    DEV_USERS = get_user_list("elevated_users.json", "6204994197")
     ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
     DEMONS = get_user_list("elevated_users.json", "2145093972")
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    TIGERS = get_user_list("elevated_users.json", "2145093972")
+    TIGERS = get_user_list("elevated_users.json", "5649200762")
     WOLVES = get_user_list("elevated_users.json", "whitelists")
     DONATION_LINK = "https://t.me/itz_legend_coder" # EG, paypal
     CERT_PATH = None
